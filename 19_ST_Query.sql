eb632d8efba56425f57aeff7f8bdc661b4b58726
dharmishthakothari <dharmishtha.only@gmail.com>
2025-10-10 08:59:17 +0530

    Add files via upload

---
+++ 19_ST_Query.sql
@@ -0,0 +1,46 @@
+-- create database 19_Sep_ST
+
+-- use 19_sep_st
+
+/* create table person(pid int,
+					pname varchar(20),
+					address varchar(200),
+					email varchar(50))
+                    
+   
+   */
+-- use 19_sep_st
+--	show tables
+-- alter table person add city varchar(20)
+-- alter table person drop email
+-- drop table person   
+
+/* create table student 
+			(fname varchar(20) , lname varchar(20) ,
+            roll_no int primary key,college_name varchar(50),address varchar(50),percen decimal)
+   */         
+-- insert into student(fname,lname,roll_no,college_name,percen,address) values('Sumaiya','Shah',301,'Gujarat University',72.34,'Kalupur')
+-- select * from student
+-- alter table student modify percen decimal(5,2)
+
+-- insert into student values('studt1','Joshi',311,'Rajshthan University','Jaipur',67.45)
+
+/* insert into student values('studt1','Joshi',401,'Assam University','City1',80.12),
+('Rohit','Bhatt',501,'Ganpati University','Mahesana',89.23),
+('Kohali','Sharma',405,'Silver oak University','Ahmedabad',78.34),
+('Surya','Kumar',504,'President College','Delhi',72.45),
+('Axar','Patel',801,'PDPU','Gabdhinagar',88.34),
+('Dhoni','Mahi',607,'HL College ','Baroda',90.23),
+('Bumarah','Jashprit',809,'Parul University','Baroda',77.34),
+('Yuvraj','Shign',909,'Punjab University','Amritsar',45.34),
+('Yujvenra','Chahal',666,'Nirma University','GN',66.23) */
+
+select * from student
+
+					
+
+
+
+
+
+                 
